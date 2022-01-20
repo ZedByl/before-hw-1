40c29f7f2759f23cd0788787eb61575e936f8ffe
Nikita Chetverikov <n.chetverikov@ylab.io>
2022-01-20 10:21:12 +0300

    Initial commit

---
+++ src/store/taskReducer.js
@@ -0,0 +1,25 @@
+import {taskDelete, taskUpdated} from "./actionTypes";
+
+export function taskReducer(state = [], action) {
+    switch (action.type) {
+        case taskUpdated: {
+            const newArray = [...state];
+            const elementIndex = newArray.findIndex(
+                (el) => el.id === action.payload.id
+            );
+            newArray[elementIndex] = {
+                ...newArray[elementIndex],
+                ...action.payload,
+            };
+            return newArray;
+        }
+        case taskDelete: {
+            const newArray = [...state];
+            const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
+            delete newArray[elementIndex];
+            return newArray;
+        }
+        default:
+            return state;
+    }
+}
